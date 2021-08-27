c2cecccf7a61f6b92e215608c0cfbaab8dcf7a5e
saranya2020b <81304177+saranya2020b@users.noreply.github.com>
2021-08-27 10:54:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
 {
-    fabric.Image.fromUrl("BirthdayImage.jpg",function(Img){
+    fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
         block_image_object=Img;
         block_image_object.scaleToWidth(700);
         block_image_object.scaleToHeight(510);
